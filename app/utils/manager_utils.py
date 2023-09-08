4eb09174c30b6370fb9c7c78af8dcefa78dc5a0f
Menny <giovanni.menon15@gmail.com>
2023-09-08 18:29:18 +0200

    Update manager_utils.py

--- app/utils/manager_utils.py
+++ app/utils/manager_utils.py
@@ -9,7 +9,7 @@ from app.utils.file.file_builder import file_chat_builder, file_translate_builde
 from app.utils.file.file_utils import extract_file_content, is_audio, path, performance, remove_selected_file, \
     save_file, compress_audio, create_path
 from app.utils.message_utils import num_tokens_from_messages, split_text_into_sections
-from app.utils.openai_utils import translate_text_call, audio_text_call, transcribe
+from app.utils.openai_utils import translate_text_call, transcribe
 from werkzeug.utils import secure_filename
 
 
@@ -48,17 +48,3 @@ def translate_manager(text, lang, filename="Utente"):
     else:
         return translate_text_call(session['LANGUAGE_OPTION_CHOOSE'], text)
 
-
-def audio_manager(text, scope, filename="Utente"):
-    if num_tokens_from_messages(text) > 500:
-        segments = split_text_into_sections(text, 350)
-        start_time = time.time()
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            audio_text_call_with_arg = functools.partial(audio_text_call, scope)
-            results = executor.map(audio_text_call_with_arg, segments)
-            audio_text = ''.join(results)
-            end_time = time.time()
-            performance(filename, session['LANGUAGE_OPTION_CHOOSE'], end_time - start_time)
-            return audio_text
-    else:
-        return audio_text_call(scope, text)
