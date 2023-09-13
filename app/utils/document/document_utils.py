724858969fc7e45b09e465818fa20430abc6ace5
Menny <giovanni.menon.dev@gmail.com>
2023-09-13 09:27:12 +0200

    Add docx support for table,header,footer

--- app/utils/document/document_utils.py
+++ app/utils/document/document_utils.py
@@ -1,7 +1,13 @@
 import concurrent.futures
 import functools
-import unicodedata
+import os
 
+import unicodedata
+import re
+from docx import Document
+from concurrent.futures import as_completed, TimeoutError
+from app.utils.file.file_utils import create_path
+from app.utils.google_utils import translate_text_with_google
 from app.utils.openai_utils import translate_document_text_call
 
 
@@ -9,13 +15,22 @@ def contains_only_punctuation(text):
     return all(unicodedata.category(char).startswith('P') for char in text)
 
 
-def docx_file_translate(parts, lang):
-    translations = {}
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+def docx_file_translate_gpt(parts, lang):
+    translations = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
         translate_call_with_lang = functools.partial(translate_document_text_call, lang)
-        results = list(executor.map(translate_call_with_lang, parts.keys()))
+        translations = list(executor.map(translate_call_with_lang, parts))
 
-    for original, translation in results:
-        translations[original] = translation
+    return translations
+
+
+def docx_file_translate_google(doc_path, lang, part):
+    doc = Document(doc_path)
+    counter = 0
+    translations = []
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        translate_call_with_lang = functools.partial(translate_text_with_google, lang)
+        translations = list(executor.map(translate_call_with_lang, part))
 
     return translations
